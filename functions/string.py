81c925fabf7e1f9f2e778078b9a4bf384edbd335
Thukten Norbu <thuktennorbu33@gmail.com>
2022-10-17 18:02:21 +0600

    second waves

---
+++ functions/string.py
@@ -0,0 +1,20 @@
+
+
+
+class String(object):
+    def __init__(self):
+        pass
+
+    def Stringembay(self):
+        bool = False
+        if type(self.para) == str:
+            print(bool)
+
+    def renthung():
+        pass
+    
+    def tsey():
+        pass
+
+l = String()
+l.Stringembay()
